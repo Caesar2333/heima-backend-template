8976a9ee68114986fdbe217abb8d980c62c8536c
caesar2334 <yxzheng1719@163.com>
2025-04-11 19:22:07 +0100

    黑马点评后端依赖更新之后的初始项目

---
+++ hm-dianping/src/main/java/com/hmdp/config/WebExceptionAdvice.java
@@ -0,0 +1,17 @@
+package com.hmdp.config;
+
+import com.hmdp.dto.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice // 这个是个异常处理的声明类
+public class WebExceptionAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public Result handleRuntimeException(RuntimeException e) {
+        log.error(e.toString(), e);
+        return Result.fail("服务器异常");
+    }
+}
